22c984f09a4c77df2fec56112830491372e93b08
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2023-05-03 09:12:15 -0500

    Create Namespaces.cpp

---
+++ implementando-ds/Namespaces.cpp
@@ -0,0 +1,25 @@
+#import <mso9.dll> no_namespace rename("DocumentProperties","DocumentPropertiesXL")
+#import <vbe6ext.olb> no_namespace
+#import <excel9.olb> 
+rename("DialogBox", "DialogBoxXL") \rename("RGB", "RBGXL") \rename("DocumentProperties", "DocumentPropertiesXL") \no_dual_interfaces
+#import <msword9.olb> 
+rename("DialogBox", "DialogBoxWord") \rename("RGB", "RBGWord") \rename("DocumentProperties", "DocumentPropertiesWord") \no_dual_interfaces
+
+namespace MyNS
+{
+  int gnCount; // Global counter
+  // Some small function implemented inline
+  double Addition(double a, double b) // Global function
+  { 
+    return a+b; 
+  }
+  // Some prototypes
+  long factorial(int n); // Global function
+  class CTest
+  {
+  public:
+    Test();
+    void Method1();
+  };
+// More variables, functions, methods, classes, structs go here
+};  
